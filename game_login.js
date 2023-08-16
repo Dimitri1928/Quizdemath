7e115c6ed0aaf84a589794da83b5b6404ddfff31
Dimitri1928 <129320871+Dimitri1928@users.noreply.github.com>
2023-08-16 14:45:09 -0300

    Add files via upload

---
+++ game_login.js
@@ -0,0 +1,12 @@
+
+function addUser() {
+  //completar a linha 4 e 5 com a palavra value
+  player1_name = document.getElementById("player1_name_input").;
+  player2_name = document.getElementById("player2_name_input").;
+
+    localStorage.setItem("player1_name", player1_name);
+    localStorage.setItem("player2_name", player2_name);
+
+    window.location = "game_page.html";
+}
+
